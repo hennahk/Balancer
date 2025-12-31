f98e7af999c32e679af2eca2d18d2a9420c8df75
hennahk <henna_b24096cs@nitc.ac.in>
2025-12-31 15:58:28 +0530

    add worker 2 server

---
+++ worker2.js
@@ -0,0 +1,9 @@
+const express= require ("express");
+const app = express();
+
+app.get("/",(req,res) => { res.send("Hi from worker 2"); } );
+
+app.get("/health",(req,res) => { res.send("OK"); } );
+
+app.listen(3001,() => {console.log("Worker 2 running on port 3001")} );
+
